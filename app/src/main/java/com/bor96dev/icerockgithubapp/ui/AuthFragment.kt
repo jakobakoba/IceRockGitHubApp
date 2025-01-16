bdb0e4c22d0e1d400b7012e2e91dd89c19e4e7ef
jakobakoba <borisgabyshev96dev@gmail.com>
2025-01-16 15:39:15 +0900

    set up viewbinding for authfragment

--- app/src/main/java/com/bor96dev/icerockgithubapp/ui/AuthFragment.kt
+++ app/src/main/java/com/bor96dev/icerockgithubapp/ui/AuthFragment.kt
@@ -1,21 +1,30 @@
 package com.bor96dev.icerockgithubapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bor96dev.icerockgithubapp.R
+import androidx.fragment.app.Fragment
+import com.bor96dev.icerockgithubapp.databinding.FragmentAuthBinding
 
 
 class AuthFragment : Fragment() {
 
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
