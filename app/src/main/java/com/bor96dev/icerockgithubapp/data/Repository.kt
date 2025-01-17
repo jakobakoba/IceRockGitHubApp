1e97bcc19a14111be6906a5bda07e6bca56127e9
jakobakoba <borisgabyshev96dev@gmail.com>
2025-01-17 10:02:01 +0900

    create repository template and include internet permission

---
+++ app/src/main/java/com/bor96dev/icerockgithubapp/data/Repository.kt
@@ -0,0 +1,16 @@
+package com.bor96dev.icerockgithubapp.data
+
+interface Repository {
+
+    suspend fun getRepositories(): List<Repo>
+
+    suspend fun getRepository(repoId: String): RepoDetails
+
+    suspend fun getRepositoryReadme(
+        ownerName: String,
+        repositoryName: String,
+        branchName: String
+    ): String
+
+    suspend fun signIn(token: String): UserInfo
+}
